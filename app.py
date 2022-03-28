722b616b331d719e756b59f3214be921220ce780
steffenhatlelid <101556450+steffenhatlelid@users.noreply.github.com>
2022-03-28 10:53:56 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -12,6 +12,27 @@ def home():
 @app.route('/app_calculator', methods=['GET', 'POST'])
 def app_calculator():
     form = AddRecordForm()
+    if form.validate_on_submit():
+        bus_kms = form.bus_kms.data
+
+        bus_type = form.bus_type.data
+
+        if bus_type == 'Bus Diesel':
+            co2_bus = float(bus_kms) * 0.10231
+        elif bus_type == 'Bus CNG':
+            co2_bus = float(bus_kms) * 0.08
+        elif bus_type == 'Bus Petrol':
+            co2_bus = float(bus_kms) * 0.10231
+        else:
+            co2_bus = float(bus_kms) * 0
+
+        co2_bus = float("{:.2f}".format(co2_bus))
+
+        form.bus_co2.data = co2_bus
+
+        return render_template('app_calculator2.html', title='App Calculator', legend='App Calculator',
+                               paragraph='(Based on the code developed by Gabriel Fuentes for the course ENE425)',
+                               co2_bus=co2_bus, bus_kms=bus_kms, bus_type=bus_type, form=form)
     return render_template('app_calculator.html', title='App Calculator', legend='App Calculator',
                            paragraph='(Based on the code developed by Gabriel Fuentes for the course ENE425)',
                            form=form)
